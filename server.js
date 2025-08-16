b6c554cd80fac73718574b69987c8946318784de
Pixel-Nesta <sunnysolanki17779@gmail.com>
2025-08-16 10:32:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-// server.js
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Serve static files (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, "public")));
-
-// Default route → index.html (Login page)
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
-
-
 
 const express = require("express");
 const bodyParser = require("body-parser");
